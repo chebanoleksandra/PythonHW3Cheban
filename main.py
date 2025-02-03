e9cfde3b77856833309e2b6dc79ee7a7e9293050
Oleksandra <alexa30che@gmail.com>
2025-02-03 11:17:28 +0200

    task 2 done

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 #1
-num1 = int(input("Input first number: "))
-num2 = int(input("Input second number: "))
-while num1 != num2:
-    if num1>num2:
-        print(f"\nIncorrect input.\nFirst number changed to {num2}\nSecond number changed to {num1}")
-        temp = num1
-        num1 = num2
-        num2 = temp
-    else:
-        print("\nOdd numbers in this range: ")
-        for i in range(num1, num2 + 1):
-            if i % 2 != 0:
-                print(i, end=" ")
-        break
-else:
-    print("\nIncorrect input. The start and the end of the range cannot be the same.")
+# num1 = int(input("Input first number: "))
+# num2 = int(input("Input second number: "))
+# while num1 != num2:
+#     if num1>num2:
+#         print(f"\nIncorrect input.\nFirst number changed to {num2}\nSecond number changed to {num1}")
+#         temp = num1
+#         num1 = num2
+#         num2 = temp
+#     else:
+#         print("\nOdd numbers in this range: ")
+#         for i in range(num1, num2 + 1):
+#             if i % 2 != 0:
+#                 print(i, end=" ")
+#         break
+# else:
+#     print("\nIncorrect input. The start and the end of the range cannot be the same.")
+
+#2
+num1 = 100
+num2 = 9999
+quantity = 0
+while num1 <= num2:
+    if num1>=1000:
+        a = num1//1000
+    b = (num1%1000)//100
+    c = (num1%100)//10
+    d = num1 % 10
+    if b!= c and b!=d and c!=d:
+        if num1>=1000:
+            if a!=b and a!= c and a!=d:
+                quantity += 1
+        else:
+            quantity+=1
+    num1+=1
+print(quantity)
